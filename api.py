1ee7e8b09372d3a92fc5faebae595e38e1f4b780
saurabhsharad <150166495+saurabhsharad@users.noreply.github.com>
2026-04-16 18:11:21 +0530

    added cors

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import joblib
 import pandas as pd
 import numpy as np
 import traceback
+import os
 
 try:
     from flask_cors import CORS
@@ -159,4 +160,5 @@ def predict():
         return jsonify({"error": str(e), "trace": traceback.format_exc()}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
